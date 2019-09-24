3d3d5d072660b496ed856b186e3040d132d442f1
Crystal Broach <crysland@gmail.com>
2019-09-23 19:51:50 -0500

    added store component with image

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,11 +1,26 @@
+const bookInfo = {
+    price: 24.99,
+    title: "Fifty Shades of Chicken",
+    image: "./assets/images/book.jpg"
+  };
+
 const printToDom = (divId, toPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = toPrint;
 };
 
+const addToCartEvent = () => {
+    console.log('added to cart');
+};
+
 const makeStore = () => {
-    const domString = 'hi new store';
+    let domString = `<h2>Our only book:</h2>`;
+    domString += `<p>Buy it now:</p>`;
+    domString += `<h3>$${bookInfo.price}</h3>`;
+    domString += `<img src=${bookInfo.image} alt="book cover"/>`;
+    domString += `<button id="cart-button" class="btn btn-info col-12">Add to Cart</button>`;
     printToDom('store-container', domString);
+    document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 };
 
 export default { makeStore };
